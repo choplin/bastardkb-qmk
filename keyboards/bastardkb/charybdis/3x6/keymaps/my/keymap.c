07dad4f59d4571adcafc9c69d69f9a7b7646d59a
Akihiro Okuno <choplin.choplin@gmail.com>
2023-11-29 14:58:12 +0900

    Disable Mod-Tap on the top right LBRC key

--- keyboards/bastardkb/charybdis/3x6/keymaps/my/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/my/keymap.c
@@ -44,7 +44,7 @@ enum charybdis_keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────────╮ ╭─────────────────────────────────────────────────────────────────────╮
-       LA_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,            KC_P,         LA_LBRC,
+       LA_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,            KC_P,         KC_LBRC,
   // ├──────────────────────────────────────────────────────────┤ ├─────────────────────────────────────────────────────────────────────┤
        LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, RCTL_T(KC_SCLN),  C_S_T(KC_RBRC),
   // ├──────────────────────────────────────────────────────────┤ ├─────────────────────────────────────────────────────────────────────┤
